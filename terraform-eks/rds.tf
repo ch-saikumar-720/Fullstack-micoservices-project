c05a568330f4a4bda03a139dc32fd97199d3d6f6
ch-saikumar-720 <158131442+ch-saikumar-720@users.noreply.github.com>
2026-02-28 11:43:04 +0530

    Remove hardcoded security group from RDS ingress

--- terraform-eks/rds.tf
+++ terraform-eks/rds.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "rds" {
     from_port       = 3306
     to_port         = 3306
     protocol        = "tcp"
-    security_groups = [aws_security_group.bastion_sg.id, "sg-082352a31c633390c"]
+    security_groups = [aws_security_group.bastion_sg.id]
   }
 
   egress {
